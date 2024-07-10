1c9688bab8f5b636b9f31b4fb337143bf6d20384
Mike Odnis <airwolf635@gmail.com>
2024-07-10 14:11:25 -0400

    chore: daily lc

---
+++ python/1598.CrawlerLogFolder.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        stack: List[str] = []
+        for log in logs:
+            # Checking if the log is a back to parent folder
+            match log:
+                case "../":
+                    if stack:
+                        stack.pop()
+                case "./":
+                    continue
+                case _:
+                    stack.append(log)
+        return len(stack)
